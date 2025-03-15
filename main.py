ac678100cf00b428ad8cb20a85829d38166a4256
hypertxt-azazin <codingprodg@gmail.com>
2025-03-15 21:31:13 +0000

    defined moves, winner & game over functions

--- main.py
+++ main.py
@@ -11,6 +11,34 @@ def print_board(board: List[List[str]]) -> None:
         print("|".join(row))
         print("-----")
 
+def is_move_valid(board: List[List[str]], row: int, col: int)-> bool:
+    """"Checks if the given move is valid or not"""
+    return board[row][col]==' '
+
+def make_move(board: List[List[str]], row: int, col: int, player) -> None:
+    """Place the player's symbol (either X or 0) at a given postition"""
+    board[row][col] = player
+
+def is_winner(board: List[List[str]], player: str) -> bool:
+    """Check if the given player has won the game"""    
+    for i in range(3):
+        if all([cell==player for cell in board[i]]) or all([board][j][i] == player for j i):
+            return True
+        if (board[0][0] == board [1][1] == board[2][2] == player or board[0][2] == board[1][1] == board[2][0]== player):
+            return True
+
+def is_game_over(board[List[List[str]]]) -> bool:
+    """Check if the game is over(Either one player wins or its a draw)"""
+    return is_winner(board, player='X') or is winner(board, player='O') or all(" " not in a row for row in board)
+
+
+                                                        
+
+
+
+
+
+
 if __name__=='__main__':
     board = create_board()
     print_board(board)
